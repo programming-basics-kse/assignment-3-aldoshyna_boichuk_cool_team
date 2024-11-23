891ff0dcd487ddb41662735e80105068ebfc917f
daldoshyna <daldoshyna@kse.org.ua>
2024-11-23 21:04:28 +0200

    changed all the print()s a bit

--- im_trying.py
+++ im_trying.py
@@ -10,6 +10,20 @@ def opening_file(fileName):
             rows.append(line)
     return header, rows
 
+def counting_medals(output_list, country, year):
+    gold_medals = 0
+    silver_medals = 0
+    bronze_medals = 0
+    for winner in output_list:
+        if winner[2] == "Gold":
+            gold_medals += 1
+        elif winner[2] == "Silver":
+            silver_medals += 1
+        elif winner[2] == "Bronze":
+            bronze_medals += 1
+    print("\nOverall {} in {} got: \nGold medals: {} \nSilver medals: {} \nBronze medals: {} \n".format(country, year, gold_medals, silver_medals, bronze_medals))
+    return output_list
+
 def medals_1(fileName, medals):
     header, rows = opening_file(fileName)
     COUNTRY = header.index('Team')
@@ -31,44 +45,30 @@ def medals_1(fileName, medals):
                 counter_year += 1
                 output_list.append([row[NAME], row[SPORT], row[MEDAL]])
     if counter_country==0:
-        print(f"\nSorry! There is no information about {country} country in our dataset.")
+        print("\nSorry! There is no information about {} country in our dataset.".format(country))
         return 0
     if counter_year==0:
-        print(f"\nSorry! There is no information about {year} year in our dataset.")
+        print("\nSorry! There is no information about {} country in {} year in our dataset.".format(country, year))
         return 0
     elif len(output_list)==0:
-        print(f"\nSorry! There were no Olympic winners from {country} in {year}.")
+        print("\nSorry! There were no Olympic winners from {} in {}.".format(country, year))
         return 0
     elif len(output_list)<10:
-        print(f"\nSorry! There were only {len(output_list)} Olympic winners from {country} in {year}.\n")
+        print("\nSorry! There were only {} Olympic winners from {} in {}.\n".format(len(output_list), country, year))
         for winner in output_list:
-            print(f"{winner[0]} - {winner[1]} - {winner[2]}")
+            print("{} - {} - {}".format(winner[0], winner[1], winner[2]))
         return counting_medals(output_list, country, year)
     else:
         for winner in output_list[:10]:
-            print(f"{winner[0]} - {winner[1]} - {winner[2]}")
+            print("{} - {} - {}".format(winner[0], winner[1], winner[2]))
         return counting_medals(output_list, country, year)
 
-
-def counting_medals(output_list, country, year):
-    gold_medals = 0
-    silver_medals = 0
-    bronze_medals = 0
-    for winner in output_list:
-        if winner[2] == "Gold":
-            gold_medals += 1
-        elif winner[2] == "Silver":
-            silver_medals += 1
-        elif winner[2] == "Bronze":
-            bronze_medals += 1
-    print(f"\nOverall {country} in {year} got: \nGold medals: {gold_medals} \nSilver medals: {silver_medals} \nBronze medals: {bronze_medals} \n")
-    return output_list
 
 
 
 parser=argparse.ArgumentParser()
-parser.add_argument("-medals", nargs=2, help="getting the country and year")
 parser.add_argument("fileName", type=str, help="getting name of file to work with")
+parser.add_argument("-medals", nargs=2, help="getting the country and year")
 parser.add_argument("-output", action='store', help="getting name to output information", dest='output')
 
 args=parser.parse_args()
